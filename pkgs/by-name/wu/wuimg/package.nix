2bba1d60c54574e0ae897b8f970e8783ee258320
Michael Daniels <mdaniels5757@gmail.com>
2025-05-11 22:39:35 -0400

    mostly working?

--- pkgs/by-name/wu/wuimg/package.nix
+++ pkgs/by-name/wu/wuimg/package.nix
@@ -2,88 +2,129 @@
   lib,
   stdenv,
   fetchgit,
+
   meson,
-  python3,
+  ninja,
   pkg-config,
-  libarchive,
-  libepoxy,
-  exiv2,
-  icu,
-  lcms2,
-  libuchardet,
-  libavif,
-  # libflif,
-  giflib,
-  libheif,
-  jbigkit,
-  libjpeg,
-  openjpeg,
+  python3,
+
   charls,
-  libjxl,
+  exiv2,
+  giflib,
+  icu,
+  jbig2dec,
+  jbigkit,
+  lcms2,
   lerc,
+  libarchive,
+  libavif,
+  libepoxy,
+  libheif,
+  libjpeg,
+  libjxl,
   libpng,
   libraw,
   librsvg,
   libtiff,
+  libuchardet,
   libwebp,
-  # libwebpdemux,
+  libxkbcommon,
+  openjpeg,
+  
+  libdrm,
+  libgbm,
+  glfw,
+  wayland,
+  wayland-protocols,
+  wayland-scanner,
+  
+  drmSupport ? (lib.meta.availableOn stdenv.hostPlatform libdrm),
+  glfwSupport ? (lib.meta.availableOn stdenv.hostPlatform glfw),
+  waylandSupport ? (lib.meta.availableOn stdenv.hostPlatform wayland)
 }:
 let
-  version = "ccfb85ded2b9f375b3a97f289239a10a06082719";
+  rev = "ccfb85ded2b9f375b3a97f289239a10a06082719";
 in
 stdenv.mkDerivation {
   pname = "wuimg";
-  inherit version;
+
+  version = rev;
 
   src = fetchgit {
     url = "https://codeberg.org/kaleido/wuimg.git";
-    rev = version;
+    rev = rev;
     hash = "sha256-3pWKqQGFwAuQNGACrsgoa8CB4f2X/lukfq5NYNVPEf4=";
   };
+
+  enableParallelBuilding = true;
+  enableDebugInfo = true;
+  dontStrip = true;
+  strictDeps = true;
+
+  mesonFlags = [
+    "-Dflif=disabled"
+    (lib.mesonEnable "window_wayland" waylandSupport)
+    (lib.mesonEnable "window_glfw" glfwSupport)
+    (lib.mesonEnable "window_drm" drmSupport)
+  ];
 
   nativeBuildInputs = [
     meson
     python3
     pkg-config
+    ninja
+  ] ++ lib.optionals (waylandSupport) [
+    wayland-scanner
   ];
 
   buildInputs = [
+    # libflif # Not availible
+
     # Required
-    libarchive
-    libepoxy
-    exiv2
-    icu
-    lcms2
-    libuchardet
-    libavif
-    # libflif
-    giflib
-    libheif
-    jbigkit
-    libjpeg
-    openjpeg
     charls
-    libjxl
+    exiv2
+    giflib
+    icu
+    jbigkit
+    lcms2
     lerc
+    libarchive
+    libavif
+    libepoxy
+    libheif
+    libjpeg
+    libjxl
     libpng
     libraw
     librsvg
     libtiff
+    libuchardet
     libwebp
-    # libwebpdemux
+    openjpeg
 
-    # For window backends you need:
-# * Wayland
-#   * libwayland
-#   * libxkbcommon
-#   * wayland-protocols
-# * DRM/KMS
-#   * libdrm
-#   * libgbm
-# * X11
-#   * libglfw3
-
+    # Undocumented
+    jbig2dec
+    # Window backends:
+  ] ++ lib.optionals (drmSupport) [
+    libdrm
+    libgbm
+  ] ++ lib.optionals (glfwSupport) [
+    glfw
+  ] ++ lib.optionals (waylandSupport) [
+    libxkbcommon
+    wayland
+    wayland-protocols
   ];
 
-  enableParallelBuilding = true;
+  patches = [
+    ./src_meson.build.patch
+    # ./window.patch
+    # ./wayland.patch
+  ];
+
+  postPatch = ''
+    substituteInPlace src/meson.build --replace-fail \
+      "/usr/share/wayland-protocols/stable" \
+      "${wayland-protocols.outPath}/share/wayland-protocols/stable"
+  '';
 }
